0c705fac6035c7c9aa31ad216cb20310bf780ef7
Yusren270 <usreniso@gmail.com>
2026-05-12 12:08:53 +0300

    Add files via upload

---
+++ 12.05.2026/12.05.2026/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12._05._2026
+{
+    internal class Program
+    {
+        static long Fibonacci(int n)
+        {
+            if (n == 1 || n == 2)
+            {
+                return 1;
+            }
+            else
+            {
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Vuvedi N-to chislo:");
+            int n = int.Parse(Console.ReadLine());
+            long fib=Fibonacci(n);
+            Console.WriteLine(fib);
+        }
+    }
+}
